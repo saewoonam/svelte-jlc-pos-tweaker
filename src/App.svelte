ff4c2e9ff4e1c24ddf3281caaa87ec1a86453cbd
Sae Woo Nam <nams@nist.gov>
2021-02-12 16:42:40 -0700

    tweak app

--- src/App.svelte
+++ src/App.svelte
@@ -11,16 +11,6 @@
 </script>
 
 <div class="App">
-  <header class="App-header">
-    <a
-      class="App-link"
-      href="https://svelte.dev"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn Svelte web page
-    </a>
-  </header>
   <p>Page has been open for <code>{count}</code> seconds.</p>
 </div>
 
